4923474cbde7f26cb3d98ed519d4530b894a03fc
Derek Critchfield <decritc@gmail.com>
2022-09-06 07:48:49 -0500

    Changing past date to use a random number of years

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -3,32 +3,33 @@ using Bogus;
 
 namespace GenericAutomapperTests.Repository
 {
-  internal class EmployeeRepository
-  {
-    public Employee GetEmployee(int employeeId)
-    {
-      var employee = new Faker<Employee>()
-        .RuleFor(o => o.Id, employeeId)
-        .RuleFor(u => u.FirstName, (f, u) => f.Name.FirstName())
-        .RuleFor(u => u.LastName, (f, u) => f.Name.LastName())
-        .RuleFor(u => u.Title, (f, u) => f.Name.JobTitle())
-        .RuleFor(u => u.StartDate, (f, u) => f.Date.Past());
+      internal class EmployeeRepository
+      {
+            private Random random = new Random();
+            public Employee GetEmployee(int employeeId)
+            {
+                var employee = new Faker<Employee>()
+                    .RuleFor(o => o.Id, employeeId)
+                    .RuleFor(u => u.FirstName, (f, u) => f.Name.FirstName())
+                    .RuleFor(u => u.LastName, (f, u) => f.Name.LastName())
+                    .RuleFor(u => u.Title, (f, u) => f.Name.JobTitle())
+                    .RuleFor(u => u.StartDate, (f, u) => f.Date.Past(random.Next(10)));
 
-      return employee;
+                return employee;
+            }
+
+            internal object GetGenericEmployee(int employeeId)
+            {
+                var employeeObject = new { Id = 0, FirstName = "", LastName = "", Title = "", StartDate = DateTime.Now};
+                  var employeeFaker = Helpers.Faker.FromAnonymousType(employeeObject)
+                    .RuleFor(o => o.Id, employeeId)
+                    .RuleFor(u => u.FirstName, (f, u) => f.Name.FirstName())
+                    .RuleFor(u => u.LastName, (f, u) => f.Name.LastName())
+                    .RuleFor(u => u.Title, (f, u) => f.Name.JobTitle())
+                    .RuleFor(u => u.StartDate, (f, u) => f.Date.Past(random.Next(10)));
+
+                  var employeeList = employeeFaker.Generate(1);
+                  return employeeList.First();
+            }
     }
-
-    internal object GetGenericEmployee(int employeeId)
-    {
-      var employeeObject = new { Id = 0, FirstName = "", LastName = "", Title = "", StartDate = DateTime.Now};
-      var employeeFaker = Helpers.Faker.FromAnonymousType(employeeObject)
-        .RuleFor(o => o.Id, employeeId)
-        .RuleFor(u => u.FirstName, (f, u) => f.Name.FirstName())
-        .RuleFor(u => u.LastName, (f, u) => f.Name.LastName())
-        .RuleFor(u => u.Title, (f, u) => f.Name.JobTitle())
-        .RuleFor(u => u.StartDate, (f, u) => f.Date.Past());
-
-      var employeeList = employeeFaker.Generate(1);
-      return employeeList.First();
-    }
-  }
 }
